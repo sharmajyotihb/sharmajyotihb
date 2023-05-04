13744f5037465e98e6e664a999568c1f51ad5c04
Krushna-23 <132591669+Krushna-23@users.noreply.github.com>
2023-05-04 21:54:39 +0530

    Routes file have added

--- patient.js
+++ patient.js
@@ -1,44 +1,6 @@
-const Patient = require("../models/patient-info");
+const express = require("express");
+const { formDataUser } = require("../controllers/patient");
+const router = express.Router();
 
-
-exports.formDataUser = (req, res )=>{
-
-//crating patient objects
-    const patient = new Patient(req.body);
-
-    //saving that patient to database 
-    
-
-   patient.save().then(patient=>{
-     
-        
-            res.json({
-                name: patient.name,
-                id: patient._id,
-                gender: patient.gender,
-                age: patient.age,
-                pno: patient.pno
-            })
-            
-        })
-        .catch(err=>{
-            return res.status(400).json({
-                err: "Not able to save patient in DB"
-            })
-        })
-       
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.post("/form", formDataUser);
+module.exports = router;
